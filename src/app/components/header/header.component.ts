c8d17ff7f80e16032f9072f87a7e2aacabdccb3b
git <martinmenalled@gmail.com>
2022-01-09 12:27:05 -0300

    Primer boton creado

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  title = 'Lista de Tareas de prueba';
+  title = 'My Task List';
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleAddTask(){
+    console.log("toogleAddTask")
+  }
 }
