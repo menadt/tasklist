ce76d46a5f78925095a27cf6a5ad513f1c2652bb
git <martinmenalled@gmail.com>
2022-01-09 12:53:14 -0300

    mock de tareas

---
+++ src/app/components/mock-task.ts
@@ -0,0 +1,28 @@
+import { Task } from "./task"
+
+export const TASKS:Task[] = [
+    {
+        id:1,
+        text:'Ver Leeds',
+        day:'10 de enero',
+        reminder:true
+    },
+    {
+        id: 2,
+        text:'estudiar',
+        day:'3 de enero',
+        reminder:true
+    },
+    {
+        id:3,
+        text:'cocinar',
+        day:'3 febrero',
+        reminder:false,
+    },
+    {
+        id:4,
+        text:'colonia',
+        day:'23 de enero',
+        reminder:true,
+    }
+]
