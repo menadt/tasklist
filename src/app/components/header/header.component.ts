8a8402a59f64337ac67c9e1ca0874b37095d173f
git <martinmenalled@gmail.com>
2022-01-09 01:03:34 -0300

    minuto 10 del video

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  title = 'Lista de Tareas de prueba';
   constructor() { }
 
   ngOnInit(): void {
